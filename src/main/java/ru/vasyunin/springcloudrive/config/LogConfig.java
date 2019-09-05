c20ce455d45bd511d304cbd80557e5d4e14024ef
Alexey Vasyunin <a.vasyunin@gmail.com>
2019-09-06 01:55:45 +0400

    Added jpa, postgresq, lombok Log requests Added user entity

---
+++ src/main/java/ru/vasyunin/springcloudrive/config/LogConfig.java
@@ -0,0 +1,17 @@
+package ru.vasyunin.springcloudrive.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class LogConfig {
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter(){
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        return loggingFilter;
+    }
+}
--- src/main/java/ru/vasyunin/springcloudrive/entity/User.java
+++ src/main/java/ru/vasyunin/springcloudrive/entity/User.java
@@ -1,7 +1,42 @@
 package ru.vasyunin.springcloudrive.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "firstname")
+    private String firstName;
+
+    @Column(name = "lastname")
+    private String lastName;
+
+    @Column(name = "photourl")
+    private String photoUrl;
+
+    @Column(name = "isactive")
+    private boolean isActive;
+
+    @Column(name = "isexpiried")
+    private boolean isExpiried;
+
+    @Column(name = "created")
+    private LocalDateTime created;
+
+    @Column(name = "lastseen")
+    private LocalDateTime lastseen;
 }
