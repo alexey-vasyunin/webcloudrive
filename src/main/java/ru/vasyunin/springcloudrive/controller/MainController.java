497fcadf206cce9ad8756d0b89b8ecc9c97fc1af
Alexey Vasyunin <a.vasyunin@gmail.com>
2019-09-05 17:25:55 +0400

    Work with template

--- src/main/java/ru/vasyunin/springcloudrive/controller/MainController.java
+++ src/main/java/ru/vasyunin/springcloudrive/controller/MainController.java
@@ -1,13 +1,18 @@
 package ru.vasyunin.springcloudrive.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class MainController {
-
     @GetMapping("/")
-    public String index(){
+    public String index(Model model, Principal principal){
+        model.addAttribute("user", principal);
         return "index";
     }
 
---
+++ src/main/java/ru/vasyunin/springcloudrive/entity/User.java
@@ -0,0 +1,7 @@
+package ru.vasyunin.springcloudrive.entity;
+
+public class User {
+    private long id;
+    private String username;
+    private String password;
+}
